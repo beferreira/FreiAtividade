f5d075e7e09c6130aa6cfbd2c678da22ad665eb7
peedroca <phm1080@gmail.com>
2024-09-23 08:35:09 -0300

    Adicionado Backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+
+const servidor = express()
+const port = 8080
+
+servidor.use(express.json())
+servidor.use(cors())
+
+servidor.get('/cards', (req, res) => {
+    let cards = [
+        {
+            titulo: 'Componentes',
+            assuntos: '- Objetos e Listas de Objetos',
+            data: '01/jan',
+            status: 'Em andamento',
+            entregaTrab: true
+        },
+        {
+            titulo: 'Componentes',
+            assuntos: '- Objetos e Listas de Objetos',
+            data: '01/jan',
+            status: 'Completo',
+            entregaTrab: true
+        }
+    ]
+
+    res.send(cards)
+})
+
+servidor.listen(port, () => console.log(`API ouvindo na porta ${port}`));
